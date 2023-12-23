44b664a34a287706c7b3ce3f86b6eaa74a172537
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-12-23 22:35:08 +0530

    Starter React + nest + tsx app

---
+++ src/pages/accordian.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function accordian() {
+  return (
+    <div>accordian</div>
+  )
+}
+
+export default accordian
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>React Essentials</title>
+      </Helmet>
+    </div>
+  );
+};
+
+export default HomePage;
