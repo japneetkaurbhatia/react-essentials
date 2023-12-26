d56fbc42afc7ac21ceba8ccb25ace24901209eae
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-12-26 23:46:33 +0530

    accordian

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,77 +1,67 @@
+import Image from 'next/image';
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
+
+const downArrow = "public/assests/down-arrow.svg";
+const upArrow = "public/assests/up-arrow.svg";
 
 const AccordionWrapper = styled.div`
   font-family: Arial, sans-serif;
   border: 1px solid #ccc;
-  border-radius: 4px;
+  border-radius: 5px;
   overflow: hidden;
-  width: 300px;
+  width: 70vw;
   max-width: 100%;
+  margin-top: 5px;
 `;
 
 const AccordionHeader = styled.div`
   background-color: #f2f2f2;
   padding: 10px;
   cursor: pointer;
-  user-select: none;
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
 const AccordionContent = styled.div`
-  padding: 10px;
 `;
 
-const Icon = styled.span`
-  font-size: 18px;
-  transition: transform 0.3s ease-in-out;
-  transform: ${(props) => (props.isExpanded ? 'rotate(90deg)' : 'rotate(0)')};
-`;
+const Accordion = ({ title = "title", children = "children"}) => {
+  const [isExpanded, setIsExpanded] = useState(false);
 
-interface AccordionProps {
-    isExpanded?: boolean,
-    tabIndex?: string,
-    contentRef?: () => {},
-    title?: string,
-    children?: string
-}
+  const toggleAccordion = () => {
+    setIsExpanded(!isExpanded);
+  };
 
-const Accordion: React.FC<AccordionProps> = ({ title, children }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const contentRef = useRef();
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      toggleAccordion();
+    }
+  };
 
-    const toggleAccordion = () => {
-        setIsExpanded((prev) => !prev);
-    };
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter' || e.key === ' ') {
-            toggleAccordion();
-        }
-    };
-
-    return (
-        <AccordionWrapper>
-            <AccordionHeader
-                role="button"
-                aria-expanded={isExpanded}
-                onClick={toggleAccordion}
-                onKeyPress={handleKeyPress}
-            >
-                {title}
-                {/* <Icon isExpanded={isExpanded}>▶</Icon> */}
-            </AccordionHeader>
-            <AccordionContent
-                ref={contentRef}
-                aria-hidden={!isExpanded}
-                // style={{ maxHeight: isExpanded ? contentRef.current.scrollHeight + 'px' : '0' }}
-            >
-                {children}
-            </AccordionContent>
-        </AccordionWrapper>
-    );
+  return (
+    <AccordionWrapper>
+      <AccordionHeader
+        role="button"
+        aria-expanded={isExpanded}
+        onClick={toggleAccordion}
+        onKeyPress={handleKeyPress}
+      >
+        {title}
+        <Image src={isExpanded ? upArrow : downArrow} alt="arrow" width="20" height="20"></Image>
+      </AccordionHeader>
+      <AccordionContent
+        aria-hidden={!isExpanded}
+        style={{
+            height: !isExpanded ? "0px" : "auto",
+            padding: !isExpanded ? "0px" : "10px",
+        }}
+      >
+        {children}
+      </AccordionContent>
+    </AccordionWrapper>
+  );
 };
 
 export default Accordion;
--- src/pages/accordian.tsx
+++ src/pages/accordian.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Accordion from '../components/Accordion';
 
-function accordian() {
-  return (
-    <div>accordian</div>
-  )
+const accordian = () => {
+  const accordionSections = [
+    ['Section 1 Heading', 'Section 1 Content Section 1 Content Section 1 Content Section 1 Content'],
+    ['Section 2 Heading', 'Section 2 Content'],
+    ['Section 3 Heading', 'Section 3 Content'],
+  ];
+
+  return <div>
+    {accordionSections.map(([title, children]) => (
+      <Accordion key={title} title={title}>
+        {children}
+      </Accordion>
+    ))}
+  </div>
 }
 
 export default accordian
