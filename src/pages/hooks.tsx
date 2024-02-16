0c86239c8850068df5ffcf24a80a35b3bb4e4740
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-16 19:38:10 +0530

    useRef FocusInput

--- src/pages/hooks.tsx
+++ src/pages/hooks.tsx
@@ -14,7 +14,8 @@ const hooks = () => {
           <StyledButton onClick={() => router.push("/hooks/useReducerHooks")}> useReducer </StyledButton>
           <StyledButton onClick={() => router.push("/hooks/useReducerWithUseContext")}> useReducerWithUseContext </StyledButton>
           <StyledButton onClick={() => router.push("/hooks/useMemoHooks")}> useMemo </StyledButton>
-          <StyledButton onClick={() => router.push("/hooks/useCallbackHooks")}> useCallbackHooks </StyledButton>
+          <StyledButton onClick={() => router.push("/hooks/useCallbackHooks")}> useCallback </StyledButton>
+          <StyledButton onClick={() => router.push("/hooks/useRefHooks")}> useRef </StyledButton>
         </ListOfComponents>
       </div>
     );
---
+++ src/pages/hooks/useRefHooks.tsx
@@ -0,0 +1,12 @@
+import FocusInput from './components/FocusInput'
+
+function useRefHooks() {
+  return (
+    <div>
+        <h1>useRefHooks</h1>
+        <FocusInput />
+    </div>
+  )
+}
+
+export default useRefHooks
