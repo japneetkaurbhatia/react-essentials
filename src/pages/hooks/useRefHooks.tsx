9cda2ae0548355f8adfbf1d8b1e63c627331767d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-16 19:47:34 +0530

    useRef HookTimer

--- src/pages/hooks/useRefHooks.tsx
+++ src/pages/hooks/useRefHooks.tsx
@@ -1,10 +1,13 @@
 import FocusInput from './components/FocusInput'
+import HookTimer from './components/HookTimer'
 
 function useRefHooks() {
   return (
     <div>
         <h1>useRefHooks</h1>
         <FocusInput />
+        <h1>HookTimer</h1>
+        <HookTimer />
     </div>
   )
 }
