098524df73180f1685f2556e5fdd50681cd22a90
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-16 14:59:52 +0530

    useReducer multiple

--- src/pages/hooks/useReducerHooks.tsx
+++ src/pages/hooks/useReducerHooks.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import UseReducerCounter from './components/UseReducerCounter'
 import UseReducerCounter2 from './components/UseReducerCounter2'
+import UseReducerCounter3 from './components/UseReducerCounter3'
 function useReducerHooks() {
   return (
     <div>
         <UseReducerCounter />
         <UseReducerCounter2 />
+        <UseReducerCounter3 />
     </div>
   )
 }
