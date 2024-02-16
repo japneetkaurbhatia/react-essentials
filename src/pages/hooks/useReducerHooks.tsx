c2646253c3b47c4130422eef0806ef2a7c9aff3d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-16 14:32:27 +0530

    useReducer simple state

---
+++ src/pages/hooks/useReducerHooks.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UseReducerCounter from './components/UseReducerCounter'
+function useReducerHooks() {
+  return (
+    <div>
+        <UseReducerCounter />
+    </div>
+  )
+}
+export default useReducerHooks
