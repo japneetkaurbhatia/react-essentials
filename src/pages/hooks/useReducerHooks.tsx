6c78e0b52a53b1860b88b0e9884142656909764d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-16 16:19:45 +0530

    data fetch using useState

--- src/pages/hooks/useReducerHooks.tsx
+++ src/pages/hooks/useReducerHooks.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import DataFetchingOne from './components/DataFetchingOne'
+import DataFetchingTwo from './components/DataFetchingTwo'
 import UseReducerCounter from './components/UseReducerCounter'
 import UseReducerCounter2 from './components/UseReducerCounter2'
 import UseReducerCounter3 from './components/UseReducerCounter3'
 function useReducerHooks() {
+
   return (
     <div>
         <UseReducerCounter />
         <UseReducerCounter2 />
         <UseReducerCounter3 />
+
+        <h1>DataFetching with useState</h1>
+        <DataFetchingOne />
+
+        <h1>DataFetching with useReducer</h1>
+        <DataFetchingTwo />
     </div>
   )
 }
