c3da45d06b42ad42d59286c3f555e11742ef52c9
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-17 23:09:15 +0530

    state management state

---
+++ src/pages/state-management/state.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+
+function State() {
+    const [name, setName] = useState("");
+  return (
+    <div>
+        <h1>State</h1>
+        <h2>name: {name}</h2>
+        <button onClick={() => setName("mnop")}>set name</button>
+    </div>
+  )
+}
+
+export default State
--- src/pages/state-management/stateVsProps.tsx
+++ src/pages/state-management/stateVsProps.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import Props from './props'
+import State from './State'
 
 function stateVsProps() {
   return (
     <div>
         <Link href={"https://www.linkedin.com/pulse/state-vs-props-react-whats-difference-mohammad-shoeb-faizan/"}>stateVsProps</Link>
+        <State />
+        <Props props={{name: "mnop"}} />
     </div>
   )
 }
