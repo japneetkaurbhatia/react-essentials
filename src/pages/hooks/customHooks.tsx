92721691dae88c50d1267fd61b80a46f6eb2b532
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-17 23:41:21 +0530

    useCounter custom hooks

--- src/pages/hooks/customHooks.tsx
+++ src/pages/hooks/customHooks.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
 import DocTitle1 from './components/DocTitle1'
 import DocTitle2 from './components/DocTitle2'
 
@@ -6,8 +8,12 @@ function customHooks() {
   return (
     <div>
         <h1>customHooks</h1>
+        <h2>Document Title</h2>
         <DocTitle1 />
         <DocTitle2 />
+        <h2>Counter</h2>
+        <CounterOne />
+        <CounterTwo />
     </div>
   )
 }
