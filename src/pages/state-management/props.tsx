5665ae95017c577cb71b339115f7b3842d71457b
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2024-02-17 23:09:23 +0530

    state management props

---
+++ src/pages/state-management/props.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Props({props}) {
+  return (
+    <div>
+        <h1>Props</h1>
+        <h2>props: {props.name}</h2>
+    </div>
+  )
+}
+
+export default Props
